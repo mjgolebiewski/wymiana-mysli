9c44bed5a301db24a35d7a794a27dc9567149ae8
Michal Golebiewski <michal.golebiewski@britenet.com.pl>
2024-01-04 16:40:16 +0100

    Job started using version:  "0.0.9"

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM docker.io/golebtom/pyflink-hello-world:v0.25
 
+ARG SCRIPT_PATH
+
 ADD --chown=flink:flink core /opt/flink/usrlib/core
-ADD --chown=flink:flink jobs/table_aggregation_example.py /opt/flink/usrlib/pyflink_job.py
+ADD --chown=flink:flink $SCRIPT_PATH /opt/flink/usrlib/pyflink_job.py
--- scripts/start_job.sh
+++ scripts/start_job.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if script path is provided
+if [ -z "$1" ]; then
+    echo "Error: Please provide the script path as an argument."
+    exit 1
+fi
+
 # Run bump-my-version to automatically increment the version
 bump-my-version bump -v patch --allow-dirty
 
@@ -9,8 +15,8 @@ CURRENT_VERSION=$(grep "current_version" pyproject.toml | sed -E 's/.*=//')
 # Set address of the Docker image
 ADDRESS=ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna
 
-# Build and push Docker image
-docker build . -t $ADDRESS:$CURRENT_VERSION
+# Build and push Docker image, passing script path as a build argument
+docker build -t $ADDRESS:$CURRENT_VERSION --build-arg SCRIPT_PATH="$1" .
 docker push $ADDRESS:$CURRENT_VERSION
 
 # Uncomment the line in kustomization.yaml
@@ -18,7 +24,8 @@ sed -i 's/^#  - pyflink-job.yaml/  - pyflink-job.yaml/' flinkjobs/kustomization.
 
 # Commit and push changes to Git
 git add .
-git commit -m "job started using version: $CURRENT_VERSION"
-git push wiosenna dev
+git commit -m "Job started using version: $CURRENT_VERSION"
+git push origin dev
 
 echo "Job started using version: $CURRENT_VERSION."
+echo "Flink UI available: https://flink.n4next.eu/"
