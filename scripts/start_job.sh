617f4015ddf7c00bd4dfe7b21d64ddc4ec4c8269
Michal Golebiewski <michal.golebiewski@britenet.com.pl>
2024-01-04 11:16:17 +0100

    version bumper setup v2

--- scripts/start_job.sh
+++ scripts/start_job.sh
@@ -7,13 +7,14 @@ bump-my-version bump -v patch
 # Extract the current version from the configuration file
 CURRENT_VERSION=$(grep "current_version" pyproject.toml | sed -E 's/.*=//')
 
-# # Build and push Docker image
-# docker build . -t ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna:$CONTAINER_VERSION
-# docker push ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna:$CONTAINER_VERSION
+# Set address of the Docker image
+ADDRESS=ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna
 
-# # Commit and push changes to Git
-# git add .
-# git commit -m "job start on version: $CONTAINER_VERSION"
-# git push wiosenna dev
+# Build and push Docker image
+docker build . -t $ADDRESS:$CONTAINER_VERSION
+docker push $ADDRESS:$CONTAINER_VERSION
 
-echo Container version: $CONTAINER_VERSION
+# Commit and push changes to Git
+git add .
+git commit -m "job start using version: $CONTAINER_VERSION"
+git push wiosenna dev
