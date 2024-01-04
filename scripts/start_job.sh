38f5a3ba80b1c1cb7bc57ddd7d7893b948d7d2af
Michal Golebiewski <michal.golebiewski@britenet.com.pl>
2024-01-04 17:10:33 +0100

    Job started using version: 0.0.13

--- scripts/start_job.sh
+++ scripts/start_job.sh
@@ -12,12 +12,15 @@ bump-my-version bump -v patch --allow-dirty
 # Extract the current version from the configuration file and trim the quotes
 CURRENT_VERSION=$(grep "current_version" pyproject.toml | sed -n 's/.*= *"\([^"]*\).*/\1/p')
 
-# Set address of the Docker image
-ADDRESS=ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna
+# Set the full address of the Docker image
+FULL_ADDRESS=ghcr.io/mjgolebiewski/wymiana-mysli/flink-wiosenna:$CURRENT_VERSION
+
+# Update the image field in the flinkjobs/pyflink-job.yaml file
+sed -i "s|image: .*|image: $FULL_ADDRESS|" flinkjobs/pyflink-job.yaml
 
 # Build and push Docker image, passing script path as a build argument
-docker build -t "$ADDRESS:$CURRENT_VERSION" --build-arg SCRIPT_PATH="$1" .
-docker push "$ADDRESS:$CURRENT_VERSION"
+docker build -t "$FULL_ADDRESS" --build-arg SCRIPT_PATH="$1" .
+docker push "$FULL_ADDRESS"
 
 # Uncomment the line in kustomization.yaml
 sed -i 's/^#  - pyflink-job.yaml/  - pyflink-job.yaml/' flinkjobs/kustomization.yaml
